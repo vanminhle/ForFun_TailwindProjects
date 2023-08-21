af3f90e8037febb867cbe20984d56ecc60c66ed0
vanminhle <vanminh.le.754@proton.me>
2023-08-21 10:48:52 +0700

    Header Styles

--- 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/components/Header.jsx
+++ 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/components/Header.jsx
@@ -8,7 +8,30 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
-      Header
+      <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
+        <Link to={'/'}>
+          <img src={Logo} alt=''></img>
+        </Link>
+        {/* Nav - hidden - desktop mode */}
+        <nav className='hidden lg:flex gap-x-12 font-semibold'>
+          <Link to={'/'} className='text-[#696c6d] hover:text-primary transition'>
+            Home
+          </Link>
+          <Link to={'/about'} className='text-[#696c6d] hover:text-primary transition'>
+            About
+          </Link>
+          <Link to={'/portfolio'} className='text-[#696c6d] hover:text-primary transition'>
+            Portfolio
+          </Link>
+          <Link to={'/contact'} className='text-[#696c6d] hover:text-primary transition'>
+            Contact
+          </Link>
+        </nav>
+        {/* socials */}
+        <Socials />
+        {/* mobile nav */}
+        <MobileNav />
+      </div>
     </header>
   );
 }
