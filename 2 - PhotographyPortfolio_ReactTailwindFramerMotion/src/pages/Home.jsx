9ecf5334a8c8b53b907d31362f3ba681a764d135
vanminhle <vanminh.le.754@proton.me>
2023-09-10 19:34:12 +0700

    Home Syles

--- 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Home.jsx
+++ 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Home.jsx
@@ -1,3 +1,33 @@
+import WomanImg from '../assets/img/home/woman.png';
+
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-  return <section className='section bg-blue-200'>Home</section>;
+  return (
+    <section className='section'>
+      <div className='container mx-auto h-full relative'>
+        {/* Text and img wrapper */}
+        <div className='flex flex-col justify-center'>
+          {/* text */}
+          <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+            <h1 className='h1'>
+              photographer <br /> & film maker
+            </h1>
+            <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>
+              Los Angeles, USA
+            </p>
+            <Link to={'/contact'} className='btn mb-[30px]'>
+              hire me
+            </Link>
+          </div>
+          {/* image */}
+          <div className='flex justify-end'>
+            <div className='relative lg:-right-40 overflow-hidden'>
+              <img src={WomanImg} alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
