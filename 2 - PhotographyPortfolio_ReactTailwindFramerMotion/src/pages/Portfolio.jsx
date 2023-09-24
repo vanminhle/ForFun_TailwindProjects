f2dac9663bd8bcbdb9240910f8cb97d7e6f3f1e8
vanminhle <vanminh.le.754@proton.me>
2023-09-24 20:11:47 +0700

    Fix Prettier TailwindCSS Formater, Portfolio Page

--- 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Portfolio.jsx
+++ 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Portfolio.jsx
@@ -1,3 +1,64 @@
+import Image1 from './../assets/img/portfolio/1.png';
+import Image2 from './../assets/img/portfolio/2.png';
+import Image3 from './../assets/img/portfolio/3.png';
+import Image4 from './../assets/img/portfolio/4.png';
+
+import { Link } from 'react-router-dom';
+
 export default function Portfolio() {
-  return <section className='section bg-green-200'>Portfolio</section>;
+  return (
+    <section className='section'>
+      <div className='container relative mx-auto h-full'>
+        <div className='flex h-full flex-col items-center justify-start gap-x-24 pb-8 pt-24 text-center lg:flex-row lg:pt-36 lg:text-left '>
+          {/* text */}
+          <div className='flex flex-col lg:items-start'>
+            <div className='h1'>Portfolio</div>
+            <p className='mb-12 max-w-sm'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, asperiores error
+              <b>odio maiores</b>quos sunt nobis ratione ut debitis dolore, magnam fugiat suscipit.
+              Inventore nostrum vitae mollitia dolor illo quos aperiam veritatis.
+              <br />
+              <br />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis quae nemo cum
+              blanditiis corporis dolores?
+            </p>
+            <Link to={'/contact'} className='btn mx-auto mb-[30px] lg:mx-0'>
+              Hire me
+            </Link>
+          </div>
+          {/* images */}
+          <div className='grid grid-cols-2 lg:gap-2'>
+            <div className='h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[320px]'>
+              <img
+                className='h-full object-cover transition-all duration-500 hover:scale-110 lg:h-[220px]'
+                src={Image1}
+                alt=''
+              />
+            </div>
+            <div className='h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[320px]'>
+              <img
+                className='h-full object-cover transition-all duration-500 hover:scale-110 lg:h-[220px]'
+                src={Image2}
+                alt=''
+              />
+            </div>
+            <div className='h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[320px]'>
+              <img
+                className='h-full object-cover transition-all duration-500 hover:scale-110 lg:h-[220px]'
+                src={Image3}
+                alt=''
+              />
+            </div>
+            <div className='h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[320px]'>
+              <img
+                className='h-full object-cover transition-all duration-500 hover:scale-110 lg:h-[220px]'
+                src={Image4}
+                alt=''
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
