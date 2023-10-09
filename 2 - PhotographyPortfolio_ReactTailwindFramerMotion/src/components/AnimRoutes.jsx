fd37a7794572da7ade6889746722ddd83250e6d5
vanminhle <vanminh.le.754@proton.me>
2023-10-09 20:20:08 +0700

    Animate precense Implementation, Home Animation

--- 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/components/AnimRoutes.jsx
+++ 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/components/AnimRoutes.jsx
@@ -2,13 +2,18 @@ import { Home, About, Portfolio, Contact } from '../pages';
 
 import { Routes, Route, useLocation } from 'react-router-dom';
 
+import { AnimatePresence } from 'framer-motion';
+
 export default function AnimRoutes() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/portfolio' element={<Portfolio />} />
-      <Route path='/contact' element={<Contact />} />
-    </Routes>
+    <AnimatePresence initial={true} mode='wait'>
+      <Routes key={location.pathname} location={location}>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
--- 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Home.jsx
+++ 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Home.jsx
@@ -2,32 +2,59 @@ import WomanImg from '../assets/img/home/woman.png';
 
 import { Link } from 'react-router-dom';
 
+import { motion } from 'framer-motion';
+
+import { transition1 } from '../transitions';
+
 export default function Home() {
   return (
-    <section className='section'>
-      <div className='container mx-auto h-full relative'>
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition1}
+      className='section'
+    >
+      <div className='container relative mx-auto h-full'>
         {/* Text and img wrapper */}
         <div className='flex flex-col justify-center'>
           {/* text */}
-          <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+          <motion.div
+            initial={{ opacity: 0, y: '-50%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-50%' }}
+            transition={transition1}
+            className='z-10 flex w-full flex-col items-center justify-center pb-14 pt-36 lg:absolute lg:w-auto lg:items-start lg:pb-0 lg:pt-0'
+          >
             <h1 className='h1'>
               photographer <br /> & film maker
             </h1>
-            <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>
+            <p className='mb-4 font-primary text-[26px] lg:mb-12 lg:text-[36px]'>
               Los Angeles, USA
             </p>
             <Link to={'/contact'} className='btn mb-[30px]'>
               hire me
             </Link>
-          </div>
+          </motion.div>
           {/* image */}
-          <div className='flex justify-end'>
-            <div className='relative lg:-right-40 overflow-hidden'>
-              <img src={WomanImg} alt='' />
-            </div>
+          <div className='flex max-h-96 justify-end lg:max-h-max'>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={transition1}
+              className='relative overflow-hidden lg:-right-40'
+            >
+              <motion.img
+                whileHover={{ scale: 1.1 }}
+                transition={{ transition1 }}
+                src={WomanImg}
+                alt=''
+              />
+            </motion.div>
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
