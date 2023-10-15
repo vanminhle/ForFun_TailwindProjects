de9d629cb56582ebadbbb9f7dba3ca57307c612b
vanminhle <vanminh.le.754@proton.me>
2023-10-15 19:06:42 +0700

    About Animation

--- 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/About.jsx
+++ 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/About.jsx
@@ -2,18 +2,34 @@ import WomanImg from '../assets/img/about/woman.png';
 
 import { Link } from 'react-router-dom';
 
+import { motion } from 'framer-motion';
+
+import { transition1 } from '../transitions';
+
 export default function About() {
   return (
-    <section className='section'>
-      <div className='container mx-auto h-full relative'>
+    <motion.section
+      initial={{ opacity: 0, y: '100%' }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: '100%' }}
+      transition={transition1}
+      className='section'
+    >
+      <div className='container relative mx-auto h-full'>
         {/* text & img wrapper */}
-        <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+        <div className='flex h-full flex-col items-center justify-center gap-x-24 text-center lg:flex-row lg:pt-16 lg:text-left'>
           {/* image */}
-          <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden'>
+          <div className='order-2 max-h-96 flex-1 overflow-hidden lg:order-none lg:max-h-max'>
             <img src={WomanImg} alt='' />
           </div>
           {/* text */}
-          <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+          <motion.div
+            initial={{ opacity: 0, y: '-80%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-80%' }}
+            transition={transition1}
+            className='z-10 flex flex-1 flex-col items-center justify-center pb-14 pt-36 lg:w-auto lg:items-start lg:pt-0'
+          >
             <h1 className='h1'>About me</h1>
             <p className='mb-12 max-w-sm'>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, asperiores error
@@ -27,9 +43,9 @@ export default function About() {
             <Link to={'/portfolio'} className='btn'>
               View my work
             </Link>
-          </div>
+          </motion.div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
