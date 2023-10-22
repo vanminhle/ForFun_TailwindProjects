5b35ab71c38a18342cf1862c78ff420733b23917
vanminhle <vanminh.le.754@proton.me>
2023-10-22 18:58:13 +0700

    Portfolio Animation

--- 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Portfolio.jsx
+++ 2 - PhotographyPortfolio_ReactTailwindFramerMotion/src/pages/Portfolio.jsx
@@ -5,13 +5,29 @@ import Image4 from './../assets/img/portfolio/4.png';
 
 import { Link } from 'react-router-dom';
 
+import { motion } from 'framer-motion';
+
+import { transition1 } from '../transitions';
+
 export default function Portfolio() {
   return (
-    <section className='section'>
+    <motion.section
+      initial={{ opacity: 0, y: '100%' }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: '100%' }}
+      transition={transition1}
+      className='section'
+    >
       <div className='container relative mx-auto h-full'>
         <div className='flex h-full flex-col items-center justify-start gap-x-24 pb-8 pt-24 text-center lg:flex-row lg:pt-36 lg:text-left '>
           {/* text */}
-          <div className='flex flex-col lg:items-start'>
+          <motion.div
+            initial={{ opacity: 0, y: '-80%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-80%' }}
+            transition={transition1}
+            className='flex flex-col lg:items-start'
+          >
             <div className='h1'>Portfolio</div>
             <p className='mb-12 max-w-sm'>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, asperiores error
@@ -25,7 +41,7 @@ export default function Portfolio() {
             <Link to={'/contact'} className='btn mx-auto mb-[30px] lg:mx-0'>
               Hire me
             </Link>
-          </div>
+          </motion.div>
           {/* images */}
           <div className='grid grid-cols-2 lg:gap-2'>
             <div className='h-[187px] max-w-[250px] overflow-hidden bg-accent lg:h-[220px] lg:max-w-[320px]'>
@@ -59,6 +75,6 @@ export default function Portfolio() {
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
