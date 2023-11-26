6c5c9c161515e994455c4e9a1dae2fb6ad013ecc
vanminhle <vanminh.le.754@proton.me>
2023-11-26 21:56:47 +0700

    Header, Styling

---
+++ 3 - SmoothPageLoader_ReactFramerMotion/src/components/header.jsx
@@ -0,0 +1,35 @@
+import { motion } from 'framer-motion';
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='header-inner'>
+        <div className='logo'>Ollie</div>
+        <nav className='nav'>
+          <li>
+            <a href='/design'>Design</a>
+          </li>
+          <li>
+            <a href='/strategy'>Strategy</a>
+          </li>
+          <li>
+            <a href='/cases'>Cases</a>
+          </li>
+          <li>
+            <a href='/about'>About</a>
+          </li>
+          <li>
+            <a href='/why'>Why work with us?</a>
+          </li>
+        </nav>
+        <div className='contact'>
+          <a href='/contact'>Let's work together</a>
+        </div>
+        <div className='hamburger-menu'>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
+}
