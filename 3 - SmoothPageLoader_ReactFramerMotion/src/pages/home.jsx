67105e1fe2c40026c858f50828a573a6a97489bb
vanminhle <vanminh.le.754@proton.me>
2023-12-10 14:03:49 +0700

    Banner Style and Animation

--- 3 - SmoothPageLoader_ReactFramerMotion/src/pages/home.jsx
+++ 3 - SmoothPageLoader_ReactFramerMotion/src/pages/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Banner } from '../components';
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Banner />
     </>
   );
 }
