45aa0970dfc694d0e22c1e17b4a963495e4898a0
vanminhle <vanminh.le.754@proton.me>
2024-02-04 13:49:08 +0700

    Svelte Beginner Movie DB App - Beginner Syntax

--- 4 - SvelteKit Beginners Movie App/sveltekitmoviedb/src/routes/+page.svelte
+++ 4 - SvelteKit Beginners Movie App/sveltekitmoviedb/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script>
+  let count = 0;
+
+  function incrementBtn() {
+    count += 1;
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Number: {count}</h2>
+<p>Visit <a href='https://kit.svelte.dev'>kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={incrementBtn}>Click to increment</button>
+<a href='/about'>Go to about</a>
+
+<style>
+    p {
+        font-size: 2rem;
+    }
+</style>
