8f83f8f4ab84d4d5dab0542cf42bf15fa0f203e0
vanminhle <vanminh.le.754@proton.me>
2024-02-18 16:28:56 +0700

    Svelte Beginner Movie DB App - PopularMovieCard

--- 4 - SvelteKit Beginners Movie App/sveltekitmoviedb/src/components/PopularMovies.svelte
+++ 4 - SvelteKit Beginners Movie App/sveltekitmoviedb/src/components/PopularMovies.svelte
@@ -1,11 +1,25 @@
 <script>
   export let movies;
   console.log(movies);
-  console.log('22:46');
+  import MovieCard from './MovieCard.svelte';
 </script>
 
 <h3>Popular Movies</h3>
 <div class='popular-movies'>
-  <div class='movie' />
-
+  {#each movies as movie}
+    <MovieCard {movie} />
+  {/each}
 </div>
+
+<style>
+    h3 {
+        padding: 0 1rem;
+    }
+
+    .popular-movies {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        grid-column-gap: 1rem;
+        grid-row-gap: 2rem;
+    }
+</style>
