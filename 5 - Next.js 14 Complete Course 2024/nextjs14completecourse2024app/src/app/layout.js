41ee6a6cd4e49d7b4d729f82be9c7b6e369e739e
vanminhle <vanminh.le.754@proton.me>
2024-03-30 20:32:41 +0700

    Next.js 14 Full Stack App Tutorial for Beginners - NextJS Components

--- 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/layout.js
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Navbar from '@/components/navbar/Navbar';
+import Footer from '@/components/footer/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,8 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
     <body className={inter.className}>
-    <h2>this is the main layout</h2>
+    <Navbar />
     {children}
+    <Footer />
     </body>
     </html>
   );
---
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import Links from '@/components/navbar/links/Links';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
+};
+export default Navbar;
