8b2ba37c75cefcfd72b04e95551247f3d161473a
vanminhle <vanminh.le.754@proton.me>
2024-04-07 13:13:51 +0700

    Next.js 14 Full Stack App Tutorial for Beginners - Navbar active link class

--- 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/components/navbar/links/Links.jsx
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/components/navbar/links/Links.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import styles from './Link.module.css';
+import NavLink from '@/components/navbar/links/navLink/navLink';
 
 const Links = () => {
   const links = [
@@ -21,12 +21,27 @@ const Links = () => {
     },
   ];
 
+  const session = true;
+  const isAdmin = true;
+
   return (
     <div className={styles.links}>
       {links.map((link =>
-          <Link href={link.path} key={link.title}>{link.title}</Link>
+          // <Link href={link.path} key={link.title}>{link.title}</Link>
+          <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin &&
+            <NavLink item={{ title: 'Admin', path: '/Admin' }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: 'Login', path: '/login' }} />
+      )}
     </div>
   );
+
 };
+
 export default Links;
