3c7236754d7e5ede16cc5a9ec17919dbe46396f7
vanminhle <vanminh.le.754@proton.me>
2024-04-11 21:28:21 +0700

    Next.js 14 Full Stack App Tutorial for Beginners - Next.JS Image Explained

--- 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/about/page.jsx
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/about/page.jsx
@@ -1,6 +1,16 @@
+import Image from 'next/image';
+import styles from './about.module.css';
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className={styles.imgContainer}>
+        {/*<Image*/}
+        {/*  src='https://images.pexels.com/photos/19845817/pexels-photo-19845817/free-photo-of-a-small-white-building-in-the-middle-of-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'*/}
+        {/*  alt='' fill />*/}
+        <Image src='/about.png' alt='' fill />
+      </div>
+    </div>
   );
 };
 export default AboutPage;
