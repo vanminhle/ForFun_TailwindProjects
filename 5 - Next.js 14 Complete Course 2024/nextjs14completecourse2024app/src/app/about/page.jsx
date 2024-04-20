c445e1765bf4e2ea8f49250493d293d31970fab4
vanminhle <vanminh.le.754@proton.me>
2024-04-20 18:01:20 +0700

    Next.js 14 Full Stack App Tutorial for Beginners - About Page Design

--- 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/about/page.jsx
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/about/page.jsx
@@ -3,12 +3,30 @@ import styles from './about.module.css';
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subTitle}>About Agency</h2>
+        <h1 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better</h1>
+        <p className={styles.desc}>Ubique viderer sagittis ne conubia placerat eum expetenda libero dictum
+          repudiandae theophrastus quaerendum utinam delicata semper ridens
+          elementum pertinax deseruisse nullam neque amet instructior sed </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        {/*<Image*/}
-        {/*  src='https://images.pexels.com/photos/19845817/pexels-photo-19845817/free-photo-of-a-small-white-building-in-the-middle-of-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'*/}
-        {/*  alt='' fill />*/}
-        <Image src='/about.png' alt='' fill />
+        <Image src='/about.png' alt='About Agency' fill className={styles.img} />
       </div>
     </div>
   );
