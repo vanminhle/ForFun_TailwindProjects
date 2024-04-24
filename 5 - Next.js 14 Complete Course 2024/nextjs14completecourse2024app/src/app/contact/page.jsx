31e4d38e65694d9bd5f1a21dbfdf720c74ca8ab0
vanminhle <vanminh.le.754@proton.me>
2024-04-24 21:27:37 +0700

    Next.js 14 Full Stack App Tutorial for Beginners - Contact Page Design

--- 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/contact/page.jsx
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/contact/page.jsx
@@ -1,6 +1,28 @@
+import Image from 'next/image';
+import styles from './contact.module.css';
+
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src='/contact.png' fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action='' className={styles.form}>
+          <input type='text' placeholder='Name or surname' />
+          <input type='text' placeholder='Email Address' />
+          <input type='phone' placeholder='Phone Number' />
+          <textarea
+            name=''
+            id=''
+            cols='30'
+            rows='10'
+            placeholder='Message'>
+          </textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   );
 };
 export default ContactPage;
