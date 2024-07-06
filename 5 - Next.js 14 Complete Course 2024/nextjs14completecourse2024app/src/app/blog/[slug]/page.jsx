135365a235b59b65b8771e14cd82d8f3ed47d27e
vanminhle <vanminh.le.754@proton.me>
2024-07-06 18:15:54 +0700

    Next.js 14 Full Stack App Tutorial for Beginners - API Route Explained

--- 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/blog/[slug]/page.jsx
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/blog/[slug]/page.jsx
@@ -5,17 +5,20 @@ import { Suspense } from 'react';
 import { getPost } from '@/lib/data';
 
 // FETCH DATA WITH AN API
-// const getData = async (slug) => {
-// 	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`, {
-// 		cache: 'no-store'
-// 	});
+const getData = async (slug) => {
+	const res = await fetch(
+		`http://localhost:3000/api/blog/${slug}`,
+		{ cache: 'no-store' },
+		{ method: 'GET' },
+	);
+	// const res = await fetch(`http://localhost:3000/api/blog/${slug}`, { method: 'DELETE' });
 
-// 	if (!res.ok) {
-// 		throw new Error('Something went wrong!');
-// 	}
+	if (!res.ok) {
+		throw new Error('Something went wrong!');
+	}
 
-// 	return res.json();
-// };
+	return res.json();
+};
 
 export const generateMetadata = async ({ params }) => {
 	const { slug } = params;
@@ -32,14 +35,14 @@ const SinglePostPage = async ({ params }) => {
 	const { slug } = params;
 
 	// FETCH DATA WITH AN API
-	// const post = await getData(slug);
+	const post = await getData(slug);
 
 	// FETCH DATA WITHOUT AN API
-	const post = await getPost(slug);
+	// const post = await getPost(slug);
 
 	return (
 		<div className={styles.container}>
-			{post.img && (
+			{post?.img && (
 				<div className={styles.imgContainer}>
 					<Image src={post?.img} alt='' fill className={styles.img} />
 				</div>
--- 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/blog/page.jsx
+++ 5 - Next.js 14 Complete Course 2024/nextjs14completecourse2024app/src/app/blog/page.jsx
@@ -3,27 +3,28 @@ import styles from './blog.module.css';
 import { getPosts } from '@/lib/data';
 
 // FETCH DATA WITH AN API
-// const getData = async () => {
-// 	const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' });
+const getData = async () => {
+	// const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' });
+	const res = await fetch('http://localhost:3000/api/blog', { cache: 'no-store' });
 
-// 	if (!res.ok) {
-// 		throw new Error('Something went wrong!');
-// 	}
+	if (!res.ok) {
+		throw new Error('Something went wrong!');
+	}
 
-// 	return res.json();
-// };
+	return res.json();
+};
 
 const BlogPage = async () => {
 	// FETCH DATA WITH AN API
-	// const posts = await getData();
+	const posts = await getData();
 
 	// FETCH DATA WITHOUT AN API
-	const posts = await getPosts();
+	// const posts = await getPosts();
 
 	return (
 		<div className={styles.container}>
 			{posts.map((post) => (
-				<div className={styles.post} key={post.id}>
+				<div className={styles.post} key={post._id}>
 					<PostCard post={post} />
 				</div>
 			))}
